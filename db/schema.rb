95cddcb1d829153b05ee690661e26fbb7ddcad8d
dominicmacaulay <dominic.62502@gmail.com>
2025-01-09 20:55:09 -0500

    create company user relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_10_003829) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_10_014653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_companies", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_user_companies_on_company_id"
+    t.index ["user_id", "company_id"], name: "index_user_companies_on_user_id_and_company_id", unique: true
+    t.index ["user_id"], name: "index_user_companies_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,4 +43,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_10_003829) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "user_companies", "companies"
+  add_foreign_key "user_companies", "users"
 end

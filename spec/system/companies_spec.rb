f37da62a34f90843b60110f191bb0bbc965f8c8a
dominicmacaulay <dominic.62502@gmail.com>
2025-01-10 00:46:42 -0500

    basic company crud tests

--- spec/system/companies_spec.rb
+++ spec/system/companies_spec.rb
@@ -2,16 +2,37 @@ require 'rails_helper'
 
 RSpec.describe 'Companies', type: :system do
   let(:user) { create(:user) }
+  let(:company) { create(:company) }
+  let!(:user_company) { create(:user_company, user:, company:) }
 
   before do
     login_as user
     visit root_path
+    click_on 'account_circle'
+    click_on 'Companies'
   end
 
   it 'can be created' do
-    click_on 'account_circle'
-    click_on 'companies'
+    name = 'New Company'
+    click_on 'New Company'
+    fill_in 'Name', with: name
+    click_on 'Create Company'
+    expect(page).to have_content("#{name} was successfully created")
+    expect(page).to have_content("#{name}")
+  end
 
-    expect(page).to have_content('Companies')
+  it 'can be edited' do
+    name = 'Updated Company'
+    click_on 'Edit'
+    fill_in 'Name', with: name
+    click_on 'Update Company'
+    expect(page).to have_content("#{name} was successfully updated")
+    expect(page).to have_content("#{name}")
+  end
+
+  it 'can be destroyed' do
+    click_on 'Edit'
+    click_on 'Delete'
+    expect(page).to have_content("#{company.name} was successfully destroyed")
   end
 end

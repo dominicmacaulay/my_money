804071702103b6ce1b4b5115ede7230511a376bf
dominicmacaulay <dominic.62502@gmail.com>
2025-01-10 12:49:13 -0500

    use the bundler setup to get access to the dropdown.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import './initializers/honeybadger'
+
+// Shoelace
+import '@shoelace-style/shoelace/dist/themes/light.css'; // Import the theme
+import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js'; // Dropdown component
+import '@shoelace-style/shoelace/dist/components/menu/menu.js'; // Menu component
+import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js'; // Menu-item component
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+
+// Set the base path to the folder you copied Shoelace's assets to
+setBasePath('/node_modules/@shoelace-style/shoelace/dist');

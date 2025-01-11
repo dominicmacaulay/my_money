cceb4e289460426fe22ff56c885977c08a916d99
dominicmacaulay <dominic.62502@gmail.com>
2025-01-11 15:19:47 -0500

    Show either the current company name or a link to create the current company in the header

--- spec/system/pages_spec.rb
+++ spec/system/pages_spec.rb
@@ -1,4 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Pages', type: :system do
+  let(:user) { create(:user) }
+
+  before do
+    login_as user
+    visit root_path
+  end
+
+  describe 'Shared header' do
+    let!(:company) { create(:company) }
+
+    context 'when user has a current company' do
+      let!(:user_company) { create(:user_company, user:, company:) }
+
+      it 'displays the company name' do
+        user.set_current_company(company)
+
+        page.refresh
+
+        expect(page).to have_content(company.name)
+      end
+    end
+
+    context 'when user does not have a current company' do
+      it 'displays a link to set the current company' do
+        expect(page).to have_link('Create Your Company')
+        expect(page).not_to have_content(company.name)
+      end
+    end
+  end
 end

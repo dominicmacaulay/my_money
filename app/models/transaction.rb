3deac1db623376c08a4d5009b273607bb7931278
dominicmacaulay <dominic.62502@gmail.com>
2025-01-15 15:43:36 -0500

    capitalize enum keys

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,11 +1,14 @@
 class Transaction < ApplicationRecord
   belongs_to :company
 
-  enum :transaction_type, [ :income, :expense ]
+  enum :transaction_type, [ :Income, :Expense ]
 
   validates :date, :amount, :transaction_type, presence: true
   validates :amount, numericality: true
   validate :amount_has_two_decimal_places
+
+  scope :income, -> { where(transaction_type: :Income) }
+  scope :expense, -> { where(transaction_type: :Expense) }
 
   private
 

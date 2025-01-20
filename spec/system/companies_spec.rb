3b131ba3339b2066dbe24f02ccb2becf59413ca0
dominicmacaulay <dominic.62502@gmail.com>
2025-01-20 13:30:04 -0500

    Fix spec

--- spec/system/companies_spec.rb
+++ spec/system/companies_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe 'Companies' do
   end
 
   it 'can be set as the current company for the app' do
-    click_on 'Set as Current Company'
+    click_on "Switch to #{company.name}"
     expect(page).to have_content "Current company set to #{company.name}"
     expect(user.reload.current_company).to eql company
   end

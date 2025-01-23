0834d5494c925a61e53c41a2420b22909e3d64c2
dominicmacaulay <dominic.62502@gmail.com>
2025-01-22 20:31:28 -0500

    Create Subcategory workflow

---
+++ app/controllers/subcategories_controller.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+class SubcategoriesController < ApplicationController
+  before_action :set_subcategory, only: %i[edit update destroy]
+
+  # GET /subcategories or /subcategories.json
+  def index
+    @subcategories = Subcategory.all
+  end
+
+  # GET /subcategories/new
+  def new
+    @subcategory = Subcategory.new
+  end
+
+  # GET /subcategories/1/edit
+  def edit; end
+
+  # POST /subcategories or /subcategories.json
+  def create
+    @subcategory = Subcategory.new(subcategory_params)
+
+    if @subcategory.save
+      redirect_to subcategories_path, notice: 'Subcategory was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+    # respond_to do |format|
+    #   if @subcategory.save
+    #     format.html { redirect_to @subcategory, notice: 'Subcategory was successfully created.' }
+    #     format.json { render :show, status: :created, location: @subcategory }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @subcategory.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
+
+  # PATCH/PUT /subcategories/1 or /subcategories/1.json
+  def update
+    if @subcategory.update(subcategory_params)
+      redirect_to subcategories_path, notice: 'Subcategory was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+    # respond_to do |format|
+    #   if @subcategory.update(subcategory_params)
+    #     format.html { redirect_to @subcategory, notice: 'Subcategory was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @subcategory }
+    #   else
+    #     format.html { render :edit, status: :unprocessable_entity }
+    #     format.json { render json: @subcategory.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
+
+  # DELETE /subcategories/1 or /subcategories/1.json
+  def destroy
+    @subcategory.destroy!
+
+    redirect_to subcategories_path, notice: 'Subcategory was successfully destroyed.'
+    # respond_to do |format|
+    #   format.html do
+    #     redirect_to subcategories_path, status: :see_other, notice: 'Subcategory was successfully destroyed.'
+    #   end
+    #   format.json { head :no_content }
+    # end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_subcategory
+    @subcategory = Subcategory.find(params.expect(:id))
+  end
+
+  # Only allow a list of trusted parameters through.
+  def subcategory_params
+    params.expect(subcategory: %i[name category_id company_id])
+  end
+end

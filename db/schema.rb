b58b22bf90b27bc3fa5d4e8d115e02cd9c25d546
dominicmacaulay <dominic.62502@gmail.com>
2026-01-27 16:27:03 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,68 +10,68 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_07_232710) do
+ActiveRecord::Schema[8.1].define(version: 2025_02_07_232710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "companies", force: :cascade do |t|
-    t.string "name", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "subcategories", force: :cascade do |t|
     t.bigint "category_id", null: false
     t.bigint "company_id", null: false
-    t.string "name", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_subcategories_on_category_id"
     t.index ["company_id"], name: "index_subcategories_on_company_id"
   end
 
   create_table "transactions", force: :cascade do |t|
+    t.decimal "amount_cents", null: false
+    t.bigint "categorizable_id"
+    t.string "categorizable_type"
+    t.bigint "company_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.date "date", null: false
     t.string "description"
-    t.decimal "amount_cents", null: false
     t.string "transaction_type", null: false
-    t.bigint "company_id", null: false
-    t.string "categorizable_type"
-    t.bigint "categorizable_id"
+    t.datetime "updated_at", null: false
     t.index ["categorizable_type", "categorizable_id"], name: "index_transactions_on_categorizable"
     t.index ["company_id"], name: "index_transactions_on_company_id"
   end
 
   create_table "user_companies", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.bigint "company_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "role", default: "admin", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["company_id"], name: "index_user_companies_on_company_id"
     t.index ["user_id", "company_id"], name: "index_user_companies_on_user_id_and_company_id", unique: true
     t.index ["user_id"], name: "index_user_companies_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.datetime "created_at", null: false
+    t.bigint "current_company_id"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
     t.datetime "updated_at", null: false
-    t.bigint "current_company_id"
     t.index ["current_company_id"], name: "index_users_on_current_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

ca587a17e93b0459ebc903847a88bfaf6ff9e75f
dominicmacaulay <dominic.62502@gmail.com>
2026-02-25 17:24:59 -0500

    update models

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,21 +6,8 @@ class User < ApplicationRecord
 
   has_many :user_companies, dependent: :destroy
   has_many :companies, through: :user_companies
-  belongs_to :current_company, class_name: 'Company', optional: true
 
   def admin_for_company?(company)
     user_companies.find_by(company:)&.admin?
-  end
-
-  def switch_current_company(company)
-    return unless companies.include?(company)
-
-    update!(current_company: company)
-  end
-
-  def handle_company_destruction(company)
-    new_company = companies.where.not(id: company.id).first
-
-    update!(current_company: new_company)
   end
 end

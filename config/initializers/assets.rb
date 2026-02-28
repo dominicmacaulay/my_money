e185bcf62cd7dae26b28599381c0ff2a620051fa
dominicmacaulay <dominic.62502@gmail.com>
2026-02-28 02:21:23 -0500

    Fix imports

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,6 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# Ensure all CSS files in subdirectories are accessible
+Rails.application.config.assets.paths << Rails.root.join("app/assets/stylesheets")

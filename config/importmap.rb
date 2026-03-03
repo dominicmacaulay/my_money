73717a35c65787cd67b9c1acf42348743a56445f
dominicmacaulay <dominic.62502@gmail.com>
2026-03-03 13:26:13 -0500

    Clean up index

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,7 @@
 
 pin "application"
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.23
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.23
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.1.200
